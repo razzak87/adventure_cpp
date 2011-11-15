d17420327bca0d2783078c9023f19a464efa7ae7
Rajib Ahmed <cool_rajib@hotmail.com>
2011-11-15 06:59:22 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rajib
+ *
+ * Created on November 15, 2011, 6:01 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
