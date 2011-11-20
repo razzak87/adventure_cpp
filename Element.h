fff69f36a411a5cfe78705cc578171d9bf886665
Rajib Ahmed <cool_rajib@hotmail.com>
2011-11-20 09:09:30 +0100

    Start adding Element/Oject that user holds

---
+++ Element.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   Element.h
+ * Author: Rajib, Razzak
+ *
+ * Created on November 20, 2011, 9:02 AM
+ */
+
+#ifndef ELEMENT_H
+#define	ELEMENT_H
+
+class Element{
+
+
+};
+#endif	/* ELEMENT_H */
